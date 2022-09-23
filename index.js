b49328169f9b8bbf17a71403af5f4e31b666b225
IvanIvanson <ghalut1977@gmail.com>
2022-09-23 16:05:15 +0300

    chenge logo

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-let barScroll = document.querySelectorAll('.progress-bar');
-let navbar = document.querySelector('.navbar');
-let navbarNav = document.querySelector(".nav-link");
-
+const barScroll = document.querySelectorAll('.progress-bar');
+const navbar = document.querySelector('.navbar');
+const navbarNav = document.querySelector(".nav-link");
+const logo = document.querySelector(".logo");
+ 
 
 window.addEventListener('scroll', function () {
-   
-    
+    let attrLogo = logo.getAttribute("src"); 
     if (window.pageYOffset > innerHeight/10) {
       navbar.classList.remove("bg-light");
       navbar.classList.add("bg-secondary");
       navbar.style.opacity = 0.9;
       navbarNav.style.color = "white";
+        // attrLogo.setAttribute("src", "./images/logo-lite.png");
+   logo.src = "./images/logo-lite.png";
+
     } else {
       navbar.classList.remove("bg-secondary");
       navbar.classList.add("bg-light");
       navbar.style.opacity = 1;
-      navbarNav.style.color = "black";
+        navbarNav.style.color = "black";
+        logo.src = "./images/logo.png";
     }
     // 
     barScroll.forEach((item) => {
